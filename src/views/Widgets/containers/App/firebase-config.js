ff00ce22119172784d9305e0e4539625ea121572
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-27 21:27:31 -0400

    add firebase config

---
+++ src/views/Widgets/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCIOo4DN9p7plURoQXrfFgm1dEDPdsM9DY",
+  authDomain: "cramm-chatboard.firebaseapp.com",
+  databaseURL: "https://cramm-chatboard.firebaseio.com",
+  projectId: "cramm-chatboard",
+  storageBucket: "cramm-chatboard.appspot.com",
+  messagingSenderId: "651037104135"
+};

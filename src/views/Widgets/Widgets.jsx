548620cb0941fa51999422e434a3d99f2763cbf7
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-27 22:17:54 -0400

    remove widget components to make room for chatboard view

--- src/views/Widgets/Widgets.jsx
+++ src/views/Widgets/Widgets.jsx
@@ -23,6 +23,7 @@ import CardAvatar from "components/Card/CardAvatar.jsx";
 import CardText from "components/Card/CardText.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
+import ChatBoardRoutes from "./routes.js"
 
 import { widgetStories, bugs, website, server } from "variables/general.jsx";
 
@@ -91,148 +92,7 @@ class Widgets extends React.Component {
     return (
       <div>
         <GridContainer>
-          <GridItem xs={12} sm={12} md={6}>
-            <Card>
-              <CardHeader color="warning" text>
-                <CardText color="warning">
-                  <h4 className={classes.cardTitleWhite}>Employees Stats</h4>
-                  <h4 className={classes.cardCategoryWhite}>
-                    New employees on 15th September, 2016
-                  </h4>
-                </CardText>
-              </CardHeader>
-              <CardBody>
-                <Table
-                  hover
-                  tableHeaderColor="warning"
-                  tableHead={["ID", "Name", "Salary", "Country"]}
-                  tableData={[
-                    ["1", "Dakota Rice", "$36,738", "Niger"],
-                    ["2", "Minerva Hooper", "$23,789", "Curaçao"],
-                    ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
-                    ["4", "Philip Chaney", "$38,735", "Korea, South"]
-                  ]}
-                />
-              </CardBody>
-            </Card>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={6}>
-            <CustomTabs
-              title="Tasks:"
-              headerColor="rose"
-              tabs={[
-                {
-                  tabName: "Bugs",
-                  tabIcon: BugReport,
-                  tabContent: (
-                    <Tasks
-                      checkedIndexes={[0, 3]}
-                      tasksIndexes={[0, 1, 2, 3]}
-                      tasks={bugs}
-                    />
-                  )
-                },
-                {
-                  tabName: "Website",
-                  tabIcon: Code,
-                  tabContent: (
-                    <Tasks
-                      checkedIndexes={[0]}
-                      tasksIndexes={[0, 1]}
-                      tasks={website}
-                    />
-                  )
-                },
-                {
-                  tabName: "Server",
-                  tabIcon: Cloud,
-                  tabContent: (
-                    <Tasks
-                      checkedIndexes={[1]}
-                      tasksIndexes={[0, 1, 2]}
-                      tasks={server}
-                    />
-                  )
-                }
-              ]}
-            />
-          </GridItem>
-        </GridContainer>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={6}>
-            <GridContainer>
-              <GridItem xs={12} sm={12} lg={6}>
-                <Card pricing>
-                  <CardBody pricing>
-                    <h6 className={classes.cardCategory}>SMALL COMPANY</h6>
-                    <div className={classes.icon}>
-                      <Home className={classes.iconRose} />
-                    </div>
-                    <h3
-                      className={`${classes.cardTitle} ${classes.marginTop30}`}
-                    >
-                      $29
-                    </h3>
-                    <p className={classes.cardDescription}>
-                      This is good if your company size is between 2 and 10
-                      Persons.
-                    </p>
-                    <Button round color="rose">
-                      Choose plan
-                    </Button>
-                  </CardBody>
-                </Card>
-              </GridItem>
-              <GridItem xs={12} sm={12} lg={6}>
-                <Card pricing plain>
-                  <CardBody pricing plain>
-                    <h6 className={classes.cardCategory}>Freelancer</h6>
-                    <div className={classes.icon}>
-                      <Icon className={classes.iconWhite}>weekend</Icon>
-                    </div>
-                    <h3
-                      className={`${classes.cardTitle} ${classes.marginTop30}`}
-                    >
-                      FREE
-                    </h3>
-                    <p className={classes.cardCategory}>
-                      This is good if your company size is between 2 and 10
-                      Persons.
-                    </p>
-                    <Button round color="white">
-                      Choose plan
-                    </Button>
-                  </CardBody>
-                </Card>
-              </GridItem>
-              <GridItem xs={12} sm={12} md={11}>
-                <Card testimonial>
-                  <div className={classes.testimonialIcon}>
-                    <FormatQuote />
-                  </div>
-                  <CardBody>
-                    <h5 className={classes.cardTestimonialDescription}>
-                      Your products, all the kits that I have downloaded from
-                      your site and worked with are sooo cool! I love the color
-                      mixtures, cards... everything. Keep up the great work!
-                    </h5>
-                  </CardBody>
-                  <CardFooter testimonial>
-                    <h4 className={classes.cardTitle}>Alec Thompson</h4>
-                    <h6 className={classes.cardCategory}>@ALECTHOMPSON</h6>
-                    <CardAvatar testimonial testimonialFooter>
-                      <a href="#pablo" onClick={e => e.preventDefault()}>
-                        <img src={image} alt="..." />
-                      </a>
-                    </CardAvatar>
-                  </CardFooter>
-                </Card>
-              </GridItem>
-            </GridContainer>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={6}>
-            <Timeline simple stories={widgetStories} />
-          </GridItem>
+          <ChatBoardRoutes />
         </GridContainer>
       </div>
     );
